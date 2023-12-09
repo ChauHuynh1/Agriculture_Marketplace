3b2df607fd8d0f052fc6b6cfe99c981afd41aa45
Huynh Chau <s3777214@rmit.edu.vn>
2023-12-09 11:25:32 +0700

    initialise the application

---
+++ Agriculture_Marketplace/app/src/main/java/com/example/agriculture_marketplace/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.agriculture_marketplace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
