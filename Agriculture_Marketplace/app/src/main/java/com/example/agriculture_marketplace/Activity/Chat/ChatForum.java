6686d870c7c832f16998627286d1e58e7d9c583f
Dutuanvu <s3924489@rmit.edu.vn>
2024-01-19 00:01:01 +0700

    update

--- Agriculture_Marketplace/app/src/main/java/com/example/agriculture_marketplace/Activity/Chat/ChatForum.java
+++ Agriculture_Marketplace/app/src/main/java/com/example/agriculture_marketplace/Activity/Chat/ChatForum.java
@@ -3,6 +3,7 @@ package com.example.agriculture_marketplace.Activity.Chat;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,6 +13,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -19,8 +21,11 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.agriculture_marketplace.Activity.BrowseForumFragment;
 import com.example.agriculture_marketplace.Activity.BrowseProductFragment;
@@ -50,6 +55,9 @@ import java.util.List;
 public class ChatForum extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+
+    DrawerLayout drawerLayout;
+    ImageView menu;
     EditText searchInput;
     ChatFragment chatFragment;
     RecyclerView recyclerView;
@@ -57,6 +65,7 @@ public class ChatForum extends AppCompatActivity {
     SearchForumRecyclerAdapter adapter;
     Button delete;
     ImageButton close;
+    LinearLayout logout, aboutUs, myForum, myProduct;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference forumCollection = db.collection("forums");
 
@@ -70,6 +79,14 @@ public class ChatForum extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_forum);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menu = findViewById(R.id.menu);
+
+        logout = findViewById(R.id.logout);
+        aboutUs= findViewById(R.id.aboutUs);
+        myForum = findViewById(R.id.my_forum);
+        myProduct = findViewById(R.id.my_product);
 
         chatFragment = new ChatFragment();
         recyclerView = findViewById(R.id.search_user_recycler_view);
@@ -103,6 +120,15 @@ public class ChatForum extends AppCompatActivity {
 
 
         delete.setOnClickListener(view -> deleteSelectedChatRoom());
+
+        menu.setOnClickListener(view -> drawerLayout.openDrawer(Gravity.LEFT));
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ChatForum.this, "Logout", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(ChatForum.this, Login.class));
+            }
+        });
 
         searchInput.setOnClickListener(new View.OnClickListener() {
             @Override
